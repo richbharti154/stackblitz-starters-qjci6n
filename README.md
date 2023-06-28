db1fc6559971f3be22dde3355b3a91de298c2af4
richbharti154 <116936700+richbharti154@users.noreply.github.com>
2023-06-28 22:17:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-qjci6n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-qjci6n)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>hii eyeryone all</h1>
+      <p>I am Richa </p>
+    </div>
+  );
+}
